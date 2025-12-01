0f0224c68fd268458bc49d4df0285b1e8e63a5e0
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-11-30 23:53:15 -0300

    testes iniciais com form

--- api/main.js
+++ api/main.js
@@ -10,53 +10,62 @@ const quartos = Array.from({ length: numeroDeQuartos }, (_, i) => ({
     hospede: "",
     cpf: "",
     checkin: "",
-    checkout: ""
+    checkout: "",
+    acao: "CHECK-IN"
 }))
 
 
 function atualizaQuarto(){
     for (let i = 0; i < quartos.length; i++) {
-    const quartoVerifica = quartos[i]
+    botoes[i].innerHTML = quartos[i].acao
 
-    if (quartoVerifica.situacao === 'ocupado') {
+    if (quartos[i].situacao === 'ocupado') {
         quartosCadastrados[i].classList.add('ocupado')
-        quartosCadastrados[i].addEventListener('click', () => {
-            console.log('Fazer Check-out no quarto: 0' + quartoVerifica.n)
-            fazerCheckOut(i)
-        })
+        quartosCadastrados[i].addEventListener('click', () => {fazerCheckOut(i)})
 
-    } else {
+    }else {
         quartosCadastrados[i].classList.add('livre')
-
-        quartosCadastrados[i].addEventListener('click', () => {
-            console.log('Fazer Check-in no quarto: 0' + quartoVerifica.n)
-            fazerCheckIn(i)
-        })
+        quartosCadastrados[i].addEventListener('click', () => {abrirForm(i)})
+    }
     }
 }
+function abrirForm(i){
+    document.querySelector('.modal').style.display = 'flex'
+    document.querySelector('#nquarto').value = quartos[i].n
+    let formulario = document.querySelector('.novo-hospede')
+    formulario.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        fazerCheckIn(i)
+    })
 }
 
 function fazerCheckIn(indiceQuarto) {
+    const nomeHospede = document.getElementById('nome').value
+    const cpfHospede = document.getElementById('cpf').value
+
     quartos[indiceQuarto].situacao = "ocupado"
-    quartos[indiceQuarto].cpf = "325.678.218-80"
-    quartos[indiceQuarto].hospede = "Renato Aragão"
+    quartos[indiceQuarto].cpf = cpfHospede
+    quartos[indiceQuarto].hospede = nomeHospede
     quartos[indiceQuarto].checkin = "21/04/2024"
+    quartos[indiceQuarto].acao = "CHECK-OUT"
+
 
     console.log(quartos[indiceQuarto])
-    botoes[indiceQuarto].innerHTML = 'CHECK-OUT'
     quartosCadastrados[indiceQuarto].classList.remove('livre')
     quartosCadastrados[indiceQuarto].classList.add('ocupado')
     atualizaQuarto()
+    document.querySelector('.modal').style.display = 'none'
 }
+
 
 function fazerCheckOut(indiceQuarto) {
     quartos[indiceQuarto].situacao = "vago"
     quartos[indiceQuarto].cpf = ""
     quartos[indiceQuarto].hospede = ""
     quartos[indiceQuarto].checkin = ""
+    quartos[indiceQuarto].acao = "CHECK-IN"
 
     console.log(quartos[indiceQuarto])
-    botoes[indiceQuarto].innerHTML = 'CHECK-IN'
     quartosCadastrados[indiceQuarto].classList.remove('ocupado')
     quartosCadastrados[indiceQuarto].classList.add('livre')
     atualizaQuarto()
