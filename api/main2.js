667320df00bb18aeb0988af56206057affee080d
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-12-01 21:00:53 -0300

    trabalhando com formulário

--- api/main2.js
+++
@@ -1,7 +0,0 @@
-const numeroDeQuartos = 5
-const quartos = [
-    {
-        nº: 1,
-        situacao: "vago"
-    }
-]
---
+++ js/api/models/Hospede.js
@@ -0,0 +1,10 @@
+export class Hospede{
+    constructor(){
+        this._nome = nome
+        this._cpf = cpf
+        this._numeroQuarto = numeroQuarto
+        this._checkin = checkin 
+        this._checkout = checkout 
+    }
+    
+}
